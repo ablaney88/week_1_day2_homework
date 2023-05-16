9c945632e3cd04e4a1d1ea262b7c0be0c8473ef1
ablaney88 <ablaney187@gmail.com>
2023-05-16 16:07:52 +0100

    second commit

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,6 +55,9 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+
+print(users["Jonathan"]["twitter"])
+
 # 2. Get Erik's hometown
 # 3. Get the list of Erik's lottery numbers
 # 4. Get the species of Avril's pet Monty
@@ -64,3 +67,5 @@ users = {
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
+
+
